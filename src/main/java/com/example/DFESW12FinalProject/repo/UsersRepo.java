d04aa1854d6f93b9c34cd7a0f7de949e41e3767a
JF <Johnhearts@outlook.com>
2022-03-15 20:04:56 +0000

    UsersRepo created

---
+++ src/main/java/com/example/DFESW12FinalProject/repo/UsersRepo.java
@@ -0,0 +1,8 @@
+package com.example.DFESW12FinalProject.repo;
+
+import com.example.DFESW12FinalProject.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepo extends JpaRepository<Users, Long> {
+
+}
