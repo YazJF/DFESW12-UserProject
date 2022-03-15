29b2da313d2fb6abffc59ca70ea3f6fe6f6204be
JF <Johnhearts@outlook.com>
2022-03-15 20:00:04 +0000

    initial commit

---
+++ src/main/java/com/example/DFESW12FinalProject/Dfesw12FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.DFESW12FinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw12FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw12FinalProjectApplication.class, args);
+	}
+
+}
