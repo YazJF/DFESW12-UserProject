8eb6d3f8003ae89dc051575168c280a9b1658276
JF <Johnhearts@outlook.com>
2022-03-16 16:31:38 +0000

    Start of controller testing

---
+++ src/test/java/com/example/DFESW12UserProject/rest/UsersControllerTest.java
@@ -0,0 +1,37 @@
+package com.example.DFESW12UserProject.rest;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:users","classpath:users-data.sql"}, executionPhase = Sql.ExecutionPhase,BEFORE_TEST_METHOD)
+@ActiveProfiles("test")
+public class UsersControllerTest {
+
+
+    @Autowired
+    private MockMvc mock;
+
+    @Autowired
+    private ObjectMapper map;
+
+    @Test
+    void testCreate() throws Exception {
+
+
+    }
+
+    @Test
+    void readAll() throws Exception {
+
+
+    }
+}
